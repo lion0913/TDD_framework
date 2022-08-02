e7276bb66c02b01a3f7cdcdff20be61a43c3095e
dajung <dajung_ju@hancom.com>
2022-08-02 10:40:02 +0900

    annotation 추가(아직 기능 없음)

---
+++ src/main/java/com/ll/exam/annotation/Controller.java
@@ -0,0 +1,5 @@
+package com.ll.exam.annotation;
+
+//@interface로 커스텀 어노테이션 정의
+public @interface Controller {
+}
--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -1,4 +1,12 @@
 package com.ll.exam.article.controller;
 
+import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.GetMapping;
+
+@Controller //Controller의 의미 : ArticleController가 controller임을 의미
 public class ArticleController {
+    @GetMapping("/usr/article/list") // /usr/article/list/free와 관련된 요청을 처리하는 함수이다.
+    public void showList() {
+
+    }
 }
